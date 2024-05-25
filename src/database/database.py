6c1adb53049caafe19968461a07e3769ad44bf34
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2024-05-25 16:07:35 -0300

    Feat: Making fruits model

--- src/database/database.py
+++ src/database/database.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
@@ -7,3 +8,18 @@ class Base(DeclarativeBase):
 
 
 db = SQLAlchemy(model_class=Base)
+
+
+class fruits(db.Model):
+    __tablename__ = "fruits"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), unique=True)
+    price: Mapped[int] = mapped_column(Integer)
+    quantity_in_stock: Mapped[int] = mapped_column(Integer)
+    description: Mapped[str] = mapped_column(String(100))
+
+    def __init__(self, name, price, quantity, description):
+        self.name = name
+        self.price = price
+        self.quantity_in_stock = quantity
+        self.description = description
